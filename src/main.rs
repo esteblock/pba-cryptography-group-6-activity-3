8c6d1cb0042d3b5c97a2d170b36fe951a948c7ff
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-05-25 15:16:13 +0800

    pseudo code

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,21 @@ mod p2_addresses;
 fn main() {
     println!("Hello, world!");
 }
+/*
+Players start with 100 points each
+VRF scheme
+commit reveal
+from rand number get number from 1 to 52 (card)
+from 1 to 52 find number from 1 to 13 (mod) (car carcd)
+from this fid bet (linear function)
+
+
+strcut
+Player
+- points
+- current card
+- random number
+
+
+*/
+
